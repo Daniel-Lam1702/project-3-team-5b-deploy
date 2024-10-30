2aaf03138b9b21073b6b46c7e51072d771df8bf6
Zach <x4ch4ri4h@gmail.com>
2024-10-29 23:29:41 -0500

    Added basic menu board link to login page

--- Front-End/src/pages/Authentication/Login.jsx
+++ Front-End/src/pages/Authentication/Login.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  return <h2>Login Page</h2>;
+  const navigate = useNavigate();
+
+  const handleMenuBoardClick = () => {
+    navigate('/menu-board'); 
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <button onClick={handleMenuBoardClick}>Go to Menu Board</button>
+    </div>
+  );
 }
 
 export default Login;
