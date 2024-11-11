6e4cc4086538c3b8d2956ddef0b3f35b9584f054
Aidan Thomas <aidanman0106@gmail.com>
2024-11-11 15:03:00 -0600

    added some google oauth functionality

--- Front-End/src/pages/Authentication/Login.jsx
+++ Front-End/src/pages/Authentication/Login.jsx
@@ -15,12 +15,47 @@ function Login() {
   }
 
   const onClickManagerHandler = () => {
+    oauthSignIn();
     navigate('/manager');
   }
 
   const onClickCustomerHandler = () => {
     navigate('/customer');
   }
+
+  /*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+function oauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '296008456961-qhf5m6rddmlddcp4fscadh9seos56jus.apps.googleusercontent.com',
+                'redirect_uri': 'https://project-3-team-5b.onrender.com/manager',
+                'response_type': 'token',
+                'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
 
   return <div className="login-page flex flex-col gap-14 justify-center items-center">
     <div className='flex gap-10'>
