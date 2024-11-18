0b1bce78dce8953696242a2739773385766aa89e
Zach <x4ch4ri4h@gmail.com>
2024-11-18 16:50:11 -0600

    Add inventory report and page functionality

---
+++ Front-End/src/pages/Manager/Inventory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InventoryReport from './InventoryReport';
+
+function Inventory() {
+  return (
+    <div>
+      <h1>Inventory Page</h1>
+      <InventoryReport />
+    </div>
+  );
+}
+
+export default Inventory;
