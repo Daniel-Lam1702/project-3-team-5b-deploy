8b62172abeb3d6d0ff9b35a3b4d7cc34d37a6e19
bbriannab <bribribb.bb@gmail.com>
2024-11-30 14:25:18 -0600

    order history outline table

--- Front-End/src/pages/Manager/OrderHistory.jsx
+++ Front-End/src/pages/Manager/OrderHistory.jsx
@@ -1,7 +1,25 @@
 function OrderHistory() {
     return (
-        <div className="bg-black text-white p-8 rounded-2xl font-bold text-3xl">
-            ORDER HISTORY
+        <div className="">
+            <h1 className="bg-black text-white p-6 mb-2 rounded-2xl font-bold text-4xl"> ORDER HISTORY</h1>
+            <table className="w-full center text-wrap justify-between bg-white">
+                <tr className="text-black text-3xl"> 
+                    <th className="">Order</th>
+                    <th className="">Menu Item</th>
+                    <th>Item Component</th>
+                    <th>Price</th>
+                    <th>Cashier Name</th>
+                    <th>Date</th>
+                </tr>
+                <tr className="text-2xl text-black w-full border-collapse">
+                    <td className="border-2 border-gray-400">1</td>
+                    <td className="">Bowl</td>
+                    <td className="">Orange Chicken, Chow Mein</td>
+                    <td className="">$30</td>
+                    <td className="">5</td>
+                    <td className="">11/29/2024</td>
+                </tr>
+            </table>
         </div>
     );
 }
